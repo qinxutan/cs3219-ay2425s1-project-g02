0244adc6c613bd00dd6a8812bbf0ab9a100d9968
thedesalizes <altaria99@gmail.com>
2024-09-19 14:38:01 +0800

    Adjust some CSS

--- frontend/src/views/AdminConsoleView.tsx
+++ frontend/src/views/AdminConsoleView.tsx
@@ -31,77 +31,79 @@ const AdminConsoleView: React.FC = () => {
   ];
     
     return (
-      <div className="w-full overflow-x-auto">
-        <h1>Admin Console</h1>
-        <Table className="w-full border-collapse border border-gray-300">
-          <TableHeader>
-            <TableRow>
-              <TableHead className="w-[100px]">Select</TableHead>
-              <TableHead>ID</TableHead>
-              <TableHead>Name</TableHead>
-              <TableHead>Age</TableHead>
-              <TableHead>Occupation</TableHead>
-              <TableHead>Location</TableHead>
-              <TableHead className="text-right">Actions</TableHead>
-            </TableRow>
-          </TableHeader>
-          <TableBody>
-            {users.map((user) => (
-              <TableRow key={user.id} style={{ borderRight: '1px black', borderBottom: '1px solid #ddd', textAlign: 'center' }}>
-                {/* Select Checkbox */}
-                <TableCell>
-                  <Checkbox.Root className="CheckboxRoot">
-                    <Checkbox.Indicator className="CheckboxIndicator">
-                      <CheckIcon />
-                    </Checkbox.Indicator>
-                  </Checkbox.Root>
-                </TableCell>
-                
-                <TableCell>{user.id}</TableCell>
-                <TableCell>{user.name}</TableCell>
-                <TableCell>{user.age}</TableCell>
-                <TableCell>{user.occupation}</TableCell>
-                <TableCell>{user.location}</TableCell>
-
-                {/* DropdownMenu for Actions */}
-                <TableCell>
-                  <DropdownMenu.Root>
-                    <DropdownMenu.Trigger asChild>
-                      <button style={{ padding: '5px', borderRadius: '4px', background: '#f0f0f0' }}>
-                        <HamburgerMenuIcon />
-                      </button>
-                    </DropdownMenu.Trigger>
-
-                    <DropdownMenu.Content
-                      sideOffset={5}
-                      style={{
-                        backgroundColor: 'white',
-                        borderRadius: '6px',
-                        padding: '5px',
-                        boxShadow: '0 2px 10px rgba(0, 0, 0, 0.15)',
-                      }}
-                    >
-                      <DropdownMenu.Item onClick={() => alert(`Viewing ${user.name}`)}>
-                        View
-                      </DropdownMenu.Item>
-                      <DropdownMenu.Item onClick={() => alert(`Editing ${user.name}`)}>
-                        Edit
-                      </DropdownMenu.Item>
-                      <DropdownMenu.Item onClick={() => alert(`Ressting ${user.name}'s password`)}>
-                        Reset password...
-                      </DropdownMenu.Item>
-                      <DropdownMenu.Item onClick={() => alert(`Deleting ${user.name}`)}>
-                        Delete
-                      </DropdownMenu.Item>
-                    </DropdownMenu.Content>
-                  </DropdownMenu.Root>
-                </TableCell>
+      <main className="admin-page">
+        <div className="w-full overflow-x-auto">
+          <h1>Admin Console</h1>
+          <Table className="w-full border-collapse border border-gray-300">
+            <TableHeader>
+              <TableRow>
+                <TableHead className="w-[100px] text-left">Select</TableHead>
+                <TableHead>ID</TableHead>
+                <TableHead>Name</TableHead>
+                <TableHead>Age</TableHead>
+                <TableHead>Occupation</TableHead>
+                <TableHead>Location</TableHead>
+                <TableHead className="text-right">Actions</TableHead>
               </TableRow>
-            ))}
-          </TableBody>
-      </Table>
-      <Pages />
-    </div>
+            </TableHeader>
+            <TableBody>
+              {users.map((user) => (
+                <TableRow key={user.id} style={{ borderRight: '1px black', borderBottom: '1px solid #ddd', textAlign: 'left' }}>
+                  {/* Select Checkbox */}
+                  <TableCell>
+                    <Checkbox.Root className="CheckboxRoot">
+                      <Checkbox.Indicator className="CheckboxIndicator">
+                        <CheckIcon />
+                      </Checkbox.Indicator>
+                    </Checkbox.Root>
+                  </TableCell>
+                  
+                  <TableCell>{user.id}</TableCell>
+                  <TableCell>{user.name}</TableCell>
+                  <TableCell>{user.age}</TableCell>
+                  <TableCell>{user.occupation}</TableCell>
+                  <TableCell>{user.location}</TableCell>
+
+                  {/* DropdownMenu for Actions */}
+                  <TableCell className='text-right'>
+                    <DropdownMenu.Root>
+                      <DropdownMenu.Trigger asChild>
+                        <button style={{ padding: '5px', borderRadius: '4px', background: '#f0f0f0'}}>
+                          <HamburgerMenuIcon />
+                        </button>
+                      </DropdownMenu.Trigger>
+
+                      <DropdownMenu.Content
+                        sideOffset={5}
+                        style={{
+                          backgroundColor: 'white',
+                          borderRadius: '6px',
+                          padding: '5px',
+                          boxShadow: '0 2px 10px rgba(0, 0, 0, 0.15)',
+                        }}
+                      >
+                        <DropdownMenu.Item onClick={() => alert(`Viewing ${user.name}`)}>
+                          View
+                        </DropdownMenu.Item>
+                        <DropdownMenu.Item onClick={() => alert(`Editing ${user.name}`)}>
+                          Edit
+                        </DropdownMenu.Item>
+                        <DropdownMenu.Item onClick={() => alert(`Ressting ${user.name}'s password`)}>
+                          Reset password...
+                        </DropdownMenu.Item>
+                        <DropdownMenu.Item onClick={() => alert(`Deleting ${user.name}`)}>
+                          Delete
+                        </DropdownMenu.Item>
+                      </DropdownMenu.Content>
+                    </DropdownMenu.Root>
+                  </TableCell>
+                </TableRow>
+              ))}
+            </TableBody>
+        </Table>
+        <Pages />
+      </div>
+    </main>
       
     );
 };
