fb7181d1554034200aa280b697066903bf3a72b2
Eugene <ekweirui@gmail.com>
2024-09-21 01:14:19 +0800

    Route all unauthenticated requests to /login page

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,12 +1,12 @@
 import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import "./App.css";
 import HomePageView from "./views/HomePageView";
 import LoginPage from "./views/LoginPageView";
 import QuestionPageView from "./views/QuestionPageView";
 import CreateAccountPageView from "./views/CreateAccountPageView";
 import CollabPageView from "./views/CollabPageView";
 import DeleteAccountPage from "./views/DeleteAccountPageView";
+import ProtectedRoute from "@/components/custom/ProtectedRoute/ProtectedRoute"; 
 
 const App: React.FC = () => {
   return (
@@ -14,10 +14,34 @@ const App: React.FC = () => {
       <Routes>
         <Route path="/" element={<HomePageView />} />
         <Route path="/login" element={<LoginPage />} />
-        <Route path="/questions" element={<QuestionPageView />} />
-        <Route path="/create-account" element={<CreateAccountPageView />} />
-        <Route path="/collab" element={<CollabPageView />} />
-        <Route path="/delete-account" element={<DeleteAccountPage />} />
+        <Route 
+          path="/questions" 
+          element={
+            <ProtectedRoute>
+              <QuestionPageView />
+            </ProtectedRoute>
+          } 
+        />
+        <Route 
+          path="/create-account" 
+          element={<CreateAccountPageView />} 
+        />
+        <Route 
+          path="/collab" 
+          element={
+            <ProtectedRoute>
+              <CollabPageView />
+            </ProtectedRoute>
+          } 
+        />
+        <Route 
+          path="/delete-account" 
+          element={
+            <ProtectedRoute>
+              <DeleteAccountPage />
+            </ProtectedRoute>
+          } 
+        />
         <Route path="*" element={<p>404: Page Not Found!</p>} />
       </Routes>
     </BrowserRouter>
