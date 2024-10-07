642be982099467a8efd5c7276a1d7aa4a796512d
qinxutan <e0505785@u.nus.edu>
2024-10-07 18:14:48 +0800

    Update

--- backend/question_service/middleware/authenticateToken.js
+++ backend/question_service/middleware/authenticateToken.js
@@ -5,12 +5,9 @@ const authenticateToken = async (req, res, next) => {
   if (token == null) return res.status(401).json({ message: 'Token required' });
 
   try {
-    // Verify the token with the user service.
-    // When running in a containerized environment, we cannot directly access host port mappings without environment variables.
-    // Therefore, unless we choose to use environment variables, we must choose between using 'http://user_service:5001/verify-token' for production or 'http://localhost:5001/verify-token' for development.
-    // In this implementation, we opt for the production URL, which means our development ways of setting up won't work with the question service anymore.
-    // Consequently, to run the application, we must use 'docker-compose up -d' instead of starting individual containers or using development commands like 'npm run dev' for the frontend and 'node server.js' for the backend.
-    const response = await fetch('http://user_service:5001/verify-token', { 
+    // Verify the token with the user service
+    const userServiceBackendUrl = process.env.USER_SERVICE_BACKEND_URL || "http://localhost:5001/verify-token";
+    const response = await fetch(userServiceBackendUrl, { 
       method: 'POST',
       headers: {
         'Authorization': `Bearer ${token}`,
