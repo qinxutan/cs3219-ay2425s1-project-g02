677c36dbd0ac3240ad18cf8f68276ab71c1ee1a4
Eugene <ekweirui@gmail.com>
2024-10-13 19:25:22 +0800

    Remove redundant CORS mapping

--- backend/user_service/app.js
+++ backend/user_service/app.js
@@ -5,14 +5,6 @@ const userRoutes = require("./routes/userRoutes");
 const firebaseConfig = require("./config/firebaseConfig"); // Ensure Firebase is initialized
 const app = express();
 
-// Allow requests from 'https://question-service-1079323726684.asia-southeast1.run.app' (cloudrun question_service) with credentials
-app.use(
-  cors({
-    origin: ["https://question-service-1079323726684.asia-southeast1.run.app"],
-    credentials: true, // Allow credentials (cookies, authorization headers, etc.)
-  })
-);
-
 // Handle preflight requests for all routes
 app.options("*", cors());
 
